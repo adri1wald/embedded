f090dd644174a7dd410cb163127cea7bc315cf8d
Adrien <adrien@genei.io>
2025-01-18 21:02:09 +0000

    feat: use a (super)pac

--- 1-blinky/src/main.rs
+++ 1-blinky/src/main.rs
@@ -1,71 +1,71 @@
 #![no_std]
 #![no_main]
 
-use core::ptr::write_volatile;
-
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use nrf52840_pac::Peripherals;
 use panic_halt as _;
+
+#[derive(Clone, Copy)]
+enum Pin {
+    P13,
+    P14,
+    P15,
+    P16,
+}
+
+use Pin::*;
+
+impl Pin {
+    fn index(&self) -> usize {
+        match self {
+            Pin::P13 => 13,
+            Pin::P14 => 14,
+            Pin::P15 => 15,
+            Pin::P16 => 16,
+        }
+    }
+
+    fn bitmask(&self) -> u32 {
+        let pin = self.index();
+        1 << pin
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    const GPIO_P0_POS: u32 = 0x5000_0000;
+    let p = Peripherals::take().expect("only called once");
 
-    const GPIO_P0_PIN13_OFFSET: u32 = 0x734;
-    const GPIO_P0_PIN14_OFFSET: u32 = 0x738;
-    const GPIO_P0_PIN15_OFFSET: u32 = 0x73C;
-    const GPIO_P0_PIN16_OFFSET: u32 = 0x740;
+    const PINS: [Pin; 4] = [P13, P14, P16, P15];
 
-    const GPIO_P0_OUTSET_OFFSET: u32 = 0x508;
-    const GPIO_P0_OUTCLR_OFFSET: u32 = 0x50C;
+    // Set all pins to high (led=off) to start
 
-    // Set all positions to high (led=off) to start
+    let mut bitmask: u32 = 0;
 
-    const GPIO_P0_OUTSET_ADDR: *mut u32 = (GPIO_P0_POS + GPIO_P0_OUTSET_OFFSET) as *mut u32;
-    const GPIO_P0_OUTCLR_ADDR: *mut u32 = (GPIO_P0_POS + GPIO_P0_OUTCLR_OFFSET) as *mut u32;
-
-    const GPIO_P0_OUT_POSITIONS: [u32; 4] = [13, 14, 16, 15];
-
-    for position in GPIO_P0_OUT_POSITIONS {
-        unsafe {
-            write_volatile(GPIO_P0_OUTSET_ADDR, (true as u32) << position);
-        }
+    for pin in PINS {
+        bitmask |= pin.bitmask();
     }
 
-    // Set P0.13-P0.16 to output
+    p.P0.outset.write(|w| unsafe { w.bits(bitmask) });
 
-    const GPIO_P0_PIN_OFFSETS: [u32; 4] = [
-        GPIO_P0_PIN13_OFFSET,
-        GPIO_P0_PIN14_OFFSET,
-        GPIO_P0_PIN15_OFFSET,
-        GPIO_P0_PIN16_OFFSET,
-    ];
+    // Configure all pins as output
 
-    const DIR_OUTPUT_POS: u32 = 0;
-    const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POS;
-
-    for offset in GPIO_P0_PIN_OFFSETS {
-        let pincnf_addr: *mut u32 = (GPIO_P0_POS + offset) as *mut u32;
-
-        unsafe {
-            // volatile write to prevent compiler from optimizing it out
-            write_volatile(pincnf_addr, PINCNF_DRIVE_LED);
-        }
+    for pin in PINS {
+        p.P0.pin_cnf[pin.index()].write(|w| w.dir().output());
     }
 
-    // Drive pins in up and down in order 13, 14, 16, 15
+    // Drive pins up and down in cycle
 
-    let mut position_index = 0;
+    let mut pin_index = 0;
+
     loop {
-        let position = GPIO_P0_OUT_POSITIONS[position_index];
-        position_index = (position_index + 1) % 4;
-        unsafe {
-            write_volatile(GPIO_P0_OUTCLR_ADDR, (true as u32) << position);
-        }
+        let pin = PINS[pin_index];
+        pin_index = (pin_index + 1) % 4;
+
+        p.P0.outclr.write(|w| unsafe { w.bits(pin.bitmask()) });
         wait();
-        unsafe {
-            write_volatile(GPIO_P0_OUTSET_ADDR, (true as u32) << position);
-        }
+
+        p.P0.outset.write(|w| unsafe { w.bits(pin.bitmask()) });
         wait();
     }
 }
