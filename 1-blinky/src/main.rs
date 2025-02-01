0745624fc007016a2050145d6e95b942a4e23174
Adrien <adrien@genei.io>
2025-02-01 23:27:30 +0000

    feat: hal time

--- 1-blinky/src/main.rs
+++ 1-blinky/src/main.rs
@@ -3,75 +3,44 @@
 
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use nrf52840_pac::Peripherals;
+use embedded_hal::digital::OutputPin;
+
+use nrf52840_hal::{
+    gpio::{self, Level, Output, Pin, PushPull},
+    pac,
+};
 use panic_halt as _;
-
-#[derive(Clone, Copy)]
-enum Pin {
-    P13,
-    P14,
-    P15,
-    P16,
-}
-
-use Pin::*;
-
-impl Pin {
-    fn index(&self) -> usize {
-        match self {
-            Pin::P13 => 13,
-            Pin::P14 => 14,
-            Pin::P15 => 15,
-            Pin::P16 => 16,
-        }
-    }
-
-    fn bitmask(&self) -> u32 {
-        let pin = self.index();
-        1 << pin
-    }
-}
 
 #[entry]
 fn main() -> ! {
-    let p = Peripherals::take().expect("only called once");
+    let p = pac::Peripherals::take().expect("only called once");
+    let port0 = gpio::p0::Parts::new(p.P0);
 
-    const PINS: [Pin; 4] = [P13, P14, P16, P15];
-
-    // Set all pins to high (led=off) to start
-
-    let mut bitmask: u32 = 0;
-
-    for pin in PINS {
-        bitmask |= pin.bitmask();
-    }
-
-    p.P0.outset.write(|w| unsafe { w.bits(bitmask) });
-
-    // Configure all pins as output
-
-    for pin in PINS {
-        p.P0.pin_cnf[pin.index()].write(|w| w.dir().output());
-    }
-
+    let mut pins: [Pin<Output<PushPull>>; 4] = [
+        port0.p0_13.into_push_pull_output(Level::High).degrade(),
+        port0.p0_14.into_push_pull_output(Level::High).degrade(),
+        port0.p0_15.into_push_pull_output(Level::High).degrade(),
+        port0.p0_16.into_push_pull_output(Level::High).degrade(),
+    ];
     // Drive pins up and down in cycle
 
     let mut pin_index = 0;
 
     loop {
-        let pin = PINS[pin_index];
-        pin_index = (pin_index + 1) % 4;
+        let pin = &mut pins[pin_index];
 
-        p.P0.outclr.write(|w| unsafe { w.bits(pin.bitmask()) });
+        let _ = pin.set_low();
 
         wait();
 
-        p.P0.outset.write(|w| unsafe { w.bits(pin.bitmask()) });
+        let _ = pin.set_high();
+
+        pin_index = (pin_index + 1) % 4;
     }
 }
 
 fn wait() {
-    for _ in 0..50_000 {
+    for _ in 0..200_000 {
         nop();
     }
 }
